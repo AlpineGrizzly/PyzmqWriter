65c10998188eb241b292429d251d29a5fa685c86
Dalton Kinney <daltonckinney@gmail.com>
2023-04-18 15:47:45 -0600

    More various changes to support unpacking zmq protocol and writing header

--- pywriter.py
+++ pywriter.py
@@ -20,7 +20,6 @@ import zmq
 import sys 
 import signal 
 
-global header_written
 def get_args(parser):
     """
     Retrieves and parse arguments from the command line.
@@ -37,7 +36,7 @@ def get_args(parser):
 
     return parser.parse_args()
 
-def handle_error(sub):
+def handle_error(sub) -> None:
     """
     Handle ZMQ SUB related errors
     """
@@ -67,7 +66,7 @@ def create_zmq_sub(zmq_pub, filter) -> int:
 
     return socket
 
-def zmq_sub_destroy(sub):
+def zmq_sub_destroy(sub) -> None:
     sub.close()        # Close the subscriber
     sub.context.term() # Terminate context 
 
@@ -83,39 +82,100 @@ def exit_handler(signum, frame):
     print("Exiting...")
     exit(0)
 
-def print_header(fh):
+def write_header(fh, outstream, ts_mode) -> bool:
     """ 
-    print_header: Print the file header from the ZMQ PUB 
-    :fh: File header information
-    """
-    print("File Header: %s\n" % fh)
+    write_header: write the file header from the ZMQ PUB to a designated stream 
 
-def unpack_zmq(socket, header_written):
+    :fh: File header information
+    :outstream: Outstream for header to be printed to
+    :ts_mode: Timestamp precision mode
+
+    return: Returns True on successfully writing header, false otherwise
+    """
+    global g_magic
+    tokens = fh.decode("utf-8").split('/') # Decode header binary + tokenize
+    
+    if len(tokens) != 5:
+        print("Error:: Insufficient header data\n")
+        return False
+
+    g_magic = int(tokens[0], 10)                 # Timing precision
+    linktype = int(tokens[1], 10)                # Link type of connection
+    thiszone = int(tokens[2], 10)                # Not sure what this is
+    sigfigs = int(tokens[3], 10)                 # Need a better verbose name
+    snaplen = int(tokens[4].rstrip('\x00'), 10)  # Snapshot length of capture
+
+    if 0xA1B2C3D4 == g_magic:
+        print("%s -> us\n" % "0xA1B2C3D4")
+
+    if 0xA1B23C4D == g_magic:
+        print("%s -> ns\n" % "0xA1B23C4D")
+
+    # Version Numbers
+    vers_major = 1
+    vers_minor = 0
+
+    # Process header
+    # Write header to outstream
+    #outstream.write(bytes(g_magic))
+    print("Magic: %lu\nLinktype: %lu\nThiszone: %lu\nSigfigs: %lu\nSnaplen: %lu\n" 
+            % (g_magic, linktype, thiszone, sigfigs, snaplen))
+
+    return True
+        
+    # magic_number_for_time/linktype/thiszone/sigfigs/snaplen
+    # Override the default and convert timestamps
+	# if(g_us_ts) { g_magic = 0xA1B2C3D4; }
+	# if(g_ns_ts) { g_magic = 0xA1B23C4D; }
+
+def write_packet(ts_msg, pkt_msg, outstream) -> None:
+    """ 
+    write_packet: Print packet data to a given outstream
+    :ts_msg: Timestamp info for packet
+    :pkt_msg: Packet content 
+    :outstream: stdout, pcap, or other file to have data written out to
+    """
+    print("Time: %s\nContent: %s\nTo outstream %s\n" % (ts_msg, pkt_msg, outstream)) #DEBUG
+
+def unpack_zmq(socket, outstream, ts_mode) -> int:
     """ 
     unpack_zmq: Unpack ZMQ PUB data 
+
     :socket: socket to receive ZMQ PUB data on 
-    :header_written: Boolean for whether or not we have written our file header already
-    return none
+    :outstream: Outstream for packet data to be printed to
+    :ts_mode: Timestamp precision mode
+
+    return: Returns 0 on success, -1 on failure
     """
-    # Receive a packet of information from ZMQ to unpacks
+    global g_header_written
+    err = 0
+    
+    # Receive a packet information from ZMQ to unpack
     dev_msg = socket.recv() # Device source
     fh_msg = socket.recv()  # File Header 
     ts_msg = socket.recv()  # Time stamp
     pkt_msg = socket.recv() # Packet content/message
     
-    if(header_written == False):
-        print_header(fh_msg)
-        return True  # We write the file header only once
+    if(g_header_written == False):
+        g_header_written = write_header(fh_msg, outstream, ts_mode)
 
-    if(dev_msg and ts_msg and pkt_msg):
-        print("Device: %s\nTime: %s\nContent:%s\n" % (dev_msg, ts_msg, pkt_msg))
+    if(g_header_written and ts_msg and pkt_msg):
+        err = write_packet(ts_msg, pkt_msg, outstream)
+        if err:
+            print("Error:: Unable to write packet data: Status Code -> %s\n" % err)
+            return -1
+        else:
+            return 0
     else:
-        print("Error:: No message data received\n")
+        print("Error:: Incomplete message/No data received\n")
+        return -1
 
 def main():
+    global g_header_written       # Initialize Global header written boolean
+    g_header_written = False      # Have we written the file header to the packet
+    ts_mode = 0                   # Timestamp Mode, 0 by default, 1 for Microsecond, 2 for Nanosecond
     filter = ""                   # Blank string means we subscribe to all topics
     outstream = sys.stdout.buffer # Initialize output stream to stdout
-    header_written = False        # Have we written the file header to the packet
 
     parser = argparse.ArgumentParser(description="Use pkt_writer.exe to save packets to a pcap file or \
         to print packets into wireshark/tshark/tcpdump.", add_help=False) # Initialize argument parser object
@@ -124,20 +184,23 @@ def main():
 
     # If a PCAP file is provided for output stream, use it instead
     if args.PCAP is not None:
-        outstream = open(args.PCAP, "w") # Write to pcap as binary file
-    
+        outstream = open(args.PCAP, "wb") # Write to pcap as binary file
+
     # Initialize ZMQ SUB
     socket = create_zmq_sub(args.ZMQ, filter)
-    
+
     if socket != -1:
     # Temp til threads implemented: while loop to catch information coming in and print to stdout. Kill it after
-        while(1): 
-            unpack_zmq(socket, header_written) # Receive and unpack zmq messages as they come in 
+        err = 0
+        while(err == 0): 
+            err = unpack_zmq(socket, outstream, ts_mode) # Receive and unpack zmq messages as they come in 
+            err = 1
     
     # Wait for the slow release of death
     
     # Shutdown the ZMQ SUB bus
-    
+    zmq_sub_destroy(socket)
+
     # Close the PCAP file if opened
     if not outstream.closed:
         print("Closing stream\n")
